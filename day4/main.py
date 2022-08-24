75fd16d72f1e03e728d47adf594c3fb83bfcd275
ajnacodes <b.daniela196@outlook.com>
2022-08-25 00:57:22 +0200

    randomisation

--- day4/main.py
+++ day4/main.py
@@ -5,6 +5,15 @@ random_integer = random.randint(1, 10)
 print(random_integer)
 
 # import a module of your own
-import my_module
-print("The information below is imported from an own module:")
-print(my_module.pi)
+# import my_module
+# print("The information below is imported from an own module:")
+# print(my_module.pi)
+
+
+# random float number
+random_float = random.random() * 10
+
+print(random_float)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}.")
