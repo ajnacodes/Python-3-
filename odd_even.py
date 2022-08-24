18a8b8b0a4c7e2948fdbc8572913ce2d23970601
ajnacodes <b.daniela196@outlook.com>
2022-08-24 17:49:46 +0200

    odd/even number checker

---
+++ odd_even.py
@@ -0,0 +1,16 @@
+# 🚨 check if a number is odd or even
+number = int(input("Which number do you want to check? "))
+
+
+
+modulo = number % 2
+if modulo == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")    
+  
+  
+#   if number % 2 == 0:
+#      print("This is an even number")
+#   else:
+#      print("This is an odd number.")
