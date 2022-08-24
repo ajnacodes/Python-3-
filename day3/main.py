3aed36d19127da64b1c10f800646c6c32d441844
ajnacodes <b.daniela196@outlook.com>
2022-08-24 20:25:46 +0200

    logical operators/multiple conditions

--- day3/main.py
+++ day3/main.py
@@ -12,17 +12,33 @@
 # extra conditional and elif
 
 
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
 
-if height >= 120:
-    print("You can ride the rollercoaster!")
-    age = int(input("What is your age? "))
-    if age < 12:
-        print("Please pay $5.")
-    elif age <= 18:
-        print("Please pay $7.")  
-    else:
-        print("Please pay $12.")      
-else: 
-    print("Sorry, you can't ride the rollercoaster.")
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age? "))
+#     if age < 12:
+#         print("Please pay $5.")
+#     elif age <= 18:
+#         print("Please pay $7.")  
+#     else:
+#         print("Please pay $12.")      
+# else: 
+#     print("Sorry, you can't ride the rollercoaster.")
+
+
+
+# logical operators
+#  if condition1 & condition2 & condition3:
+#       do this
+#  else:
+#       do this
+
+
+#   A and B  - both of them must be true
+#    C or D - one of them to be true/none to be false
+#     not E - requires not E in order to be true - 
+    #   not - example: 
+    # age = 12
+    # not age > 15 - this will return true, as it reads: age not bigger than 15
