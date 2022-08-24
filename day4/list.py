5f0c6da03b193f250d7f0d878c28e998fd89fad7
ajnacodes <b.daniela196@outlook.com>
2022-08-25 01:51:05 +0200

    lists

---
+++ day4/list.py
@@ -0,0 +1,10 @@
+# lists are a way of storing and organising data 
+# fruits = ["Cherries", "Apples", "Pears"]
+# list of states in America
+usa_states = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "New York",
+              "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia",
+              "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana",
+              "Indiana", "Texas", "Iowa", "Wisconsin", "Alabama", "Maine", "Missouri", 
+              "Arkansas", "Michigan", "Florida", "Nevada", "Colorado", "Alaska", "Hawaii"]
+
+print(usa_states[2])
