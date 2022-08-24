6f371b60b20602e9f09cc059402cb6be40a6eeac
ajnacodes <b.daniela196@outlook.com>
2022-08-25 00:50:07 +0200

    import own module .py file

--- day4/main.py
+++ day4/main.py
@@ -3,3 +3,8 @@ import random
 
 random_integer = random.randint(1, 10)
 print(random_integer)
+
+# import a module of your own
+import my_module
+print("The information below is imported from an own module:")
+print(my_module.pi)
