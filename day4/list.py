b247fc4fbb9bc3370dd4378f7b08e5ea24c16027
ajnacodes <b.daniela196@outlook.com>
2022-08-25 01:54:17 +0200

    comment

--- day4/list.py
+++ day4/list.py
@@ -8,5 +8,6 @@ usa_states = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut"
               "Arkansas", "Michigan", "Florida", "Nevada", "Colorado", "Alaska", "Hawaii"]
 
 print(usa_states[2])
+# change the content of an individual state
 usa_states[-2] = "COLORADO"
 print(usa_states[-2])
