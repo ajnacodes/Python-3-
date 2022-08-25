ac69baf238274cc36b23dd330ac6c7b0f5077515
ajnacodes <b.daniela196@outlook.com>
2022-08-25 22:26:48 +0200

    even numbers calc between 1 100

--- day5/forloop.py
+++ day5/forloop.py
@@ -15,3 +15,10 @@ total = 0
 for number in range(1, 101):
     total += number
 print(total)
+
+# add upp even numbers range 1 to 100
+total = 0
+for number in range(2, 101, 2):
+    total += number
+
+print(total)
