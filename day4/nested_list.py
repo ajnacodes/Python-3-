82815cee31baf1658cd9d2d41a4b0ef6de71df7e
ajnacodes <b.daniela196@outlook.com>
2022-08-25 13:25:55 +0200

    nested list item

--- day4/nested_list.py
+++ day4/nested_list.py
@@ -2,5 +2,5 @@ fruits = ["Strawberries", "Nectarines", "Apples", "Cherries", "Peaches"]
 vegetables = ["Spinach", "Kale", "Tomatoes"]
 
 nested_list = [fruits, vegetables]
-
-print(nested_list)
+# here we will print the third element (2) from the first list (0)
+print(nested_list[0][2])
