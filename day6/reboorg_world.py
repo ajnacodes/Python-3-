ba88f53adb21e84a5e67350c0564e22432265a20
ajnacodes <b.daniela196@outlook.com>
2022-08-26 09:31:28 +0200

    Reeborg's World - Create Path for Robot

---
+++ day6/reboorg_world.py
@@ -0,0 +1,28 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jumpp():
+#    turn_left()
+#    move()
+ 
+# def step():
+#     move()
+#     jumpp()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# step()
+# step()
+# step()
+# step()
+# step()
+# step()
+
+
+# link:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
