d4d24781c3fb44bef1a536d74ea980dd624dc052
ajnacodes <b.daniela196@outlook.com>
2022-08-26 12:28:21 +0200

    Reeborg's World: jump over walls no matter height

---
+++ day6/3reeborg_world.py
@@ -0,0 +1,32 @@
+# Reboorg's World
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+# jump over obstacles and continue walking on path until destination is reached
+# jump over walls no matter what height they have
+
+# code
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def step():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+    
+# def path():
+#     while not at_goal():
+#         if wall_in_front():
+#             step()
+#         else:
+#             move()
+
+# path()
