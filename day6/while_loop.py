25c62bb847f7c31ba0e361476aad409778ed4970
ajnacodes <b.daniela196@outlook.com>
2022-08-26 10:16:01 +0200

    while loop

---
+++ day6/while_loop.py
@@ -0,0 +1,17 @@
+# while loops
+# while something, do something else
+# while something_is_true
+#   do something repeatedly
+
+
+# print a sentence 10 times
+# define counter as 0
+counter = 0
+
+# define the while loop
+while counter < 10:
+    # increase the counter with 1, so that it doesn't go infinite
+    counter += 1
+    print(f"This is the loop number : {counter}")
+    
+    
