8ec7aa676335ec9222bc370997abee5c5c8f03e9
ajnacodes <b.daniela196@outlook.com>
2022-08-26 11:18:29 +0200

    Reboorg's World: path that works on any path given

---
+++ day6/2reboorg_world.py
@@ -0,0 +1,44 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+# Reboorg's World exercise: 
+# create a function that will jump over walls, move forward, or/and stop at the finish line
+# *the function must work on ANY path!
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jumpp():
+#    turn_left()
+#    move()
+ 
+# def step():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# def path():
+#     while not at_goal():
+#         if front_is_clear() == 1:
+#             move()
+#             if wall_in_front() == 1:
+#                 step()
+#                 if front_is_clear() == 1:
+#                     move()
+#                     if wall_in_front() == 1:
+#                         step()
+#                     elif front_is_clear() == 1:
+#                         move()
+#         elif wall_in_front() == 1:
+#             step()
+
+        
+    
+    
+# path()
