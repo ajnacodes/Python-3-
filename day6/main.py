b23995e49dce3230dc1833269bc442625f428117
ajnacodes <b.daniela196@outlook.com>
2022-08-26 09:43:14 +0200

    for loop using range

--- day6/main.py
+++ day6/main.py
@@ -2,3 +2,12 @@ print("Hello")
 # built-in function: len() will return the length of the string
 num_char = len("Hello")
 print(num_char)
+
+# loop a function multiple times using range()
+
+number = 0
+
+for number in range(7):
+    number += 1
+    print(f"This is loop number {number}.")
+
