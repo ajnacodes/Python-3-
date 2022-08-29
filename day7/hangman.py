dcbed681f2697ef370e8772d5793504bb1afde79
ajnacodes <b.daniela196@outlook.com>
2022-08-29 11:25:40 +0200

    Hangman Game - Done

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,62 +1,7 @@
 import random
-
+from hangman_words import word_list
 # add stages
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+from hangman_art import stages
 # define game status
 end_of_game = False
 # define words list
@@ -68,8 +13,10 @@ chosen_word = random.choice(word_list)
 lives = 6
 stages[lives]
 
+from hangman_art import logo
+print(logo)
 
-print(f"The word is:{chosen_word}.")
+# print(f"The word is:{chosen_word}.")
 
 
 # create an empty list, then add a blank space to it for each letter in the chosen word
@@ -82,8 +29,13 @@ for letter in chosen_word:
 # function that will keep the game running until no blank spaces are encountered
 
 while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"You've already guessed the letter {guess}.")
+        
     if "_" in display:
-        guess = input("Guess a letter: ").lower()
+        
         # loop through the chosen word and replace letters if the guess matches
         for space in range(len(chosen_word)):
             letter = chosen_word[space]
@@ -92,23 +44,20 @@ while not end_of_game:
         # join all the elements in the list and turn it into a string
         print(f"{' '.join(display)}")
         
+    
+        
     # set end of game depending on the lives left
     if guess not in chosen_word:
         lives -= 1
         print(stages[lives])
+        print(f"You guessed {guess}, that's not in the word. Lives left: {lives}.")
     elif guess in chosen_word:
         print(stages[lives])
         
     if lives == 0:
         end_of_game = True
         print("You lose. No more lives.")
-
-
-
-                
-
-       
-
+        print(f"The word was: {chosen_word}.")
 
     if "_" not in display:
         print("You win!")
